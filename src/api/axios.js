aca2cf125cf3723a08226fac6e69c9290797fa34
Nuredin05 <mada4everlove@gmail.com>
2026-04-07 11:12:59 +0300

    full functional

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 // Add a request interceptor to include the JWT token
 api.interceptors.request.use(
